19244865f08fa39471e1966625775c04bb7af48e
FraFux <franz.muncke@live.de>
2018-11-02 14:23:02 +0100

    writefile

--- js/services.js
+++ js/services.js
@@ -1,4 +1,4 @@
-angular.module( 'ATEM-App.services', [] )
+angular.module('ATEM-App.services', [])
   // .factory( 'cordovaReady', [ function() {
   //   return function( fn ) {
   //     var queue = [],
@@ -19,7 +19,7 @@ angular.module( 'ATEM-App.services', [] )
   //   };
   // } ] )
 
-  .service( 'fileService', [ '$http', function( $http ) {
+  .service('fileService', ['$http', function($http) {
     var files = [];
     var sensorBuffer = [];
     const SENSORID = 0;
@@ -32,83 +32,83 @@ angular.module( 'ATEM-App.services', [] )
     var sensorInterval;
     var filesystem;
 
-    window.addEventListener( 'devicemotion', function( event ) {
-      x = event.accelerationIncludingGravity.x.toString().replace( ".", "," );
-      y = event.accelerationIncludingGravity.y.toString().replace( ".", "," );
-      z = event.accelerationIncludingGravity.z.toString().replace( ".", "," );
-    } );
+    window.addEventListener('devicemotion', function(event) {
+      x = event.accelerationIncludingGravity.x.toString().replace(".", ",");
+      y = event.accelerationIncludingGravity.y.toString().replace(".", ",");
+      z = event.accelerationIncludingGravity.z.toString().replace(".", ",");
+    });
 
     function requestFS() {
-      window.requestFileSystem( LocalFileSystem.PERSISTENT, 0, function( fs ) {
+      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
         filesystem = fs;
-      } );
+      });
     }
 
-    function logEvent( logText, component, item ) {
-      if ( files.length > 0 ) {
+    function logEvent(logText, component, item) {
+      if (files.length > 0) {
         var timestamp = Date.now() - startTime;
-        if ( component > 0 )
-          writeFile( files[ EVENTID ], timestamp + ': Component ' + component + ', Item ' + item + ': ' + logText + '\n', true );
-        else writeFile( files[ EVENTID ], timestamp + ': ' + logText + '\n', true );
+        if (component > 0)
+          writeFile(files[EVENTID], timestamp + ': Component ' + component + ', Item ' + item + ': ' + logText + '\n', true);
+        else writeFile(files[EVENTID], timestamp + ': ' + logText + '\n', true);
       }
     }
 
     function logSensor() {
-      if ( files.length > 0 ) {
+      if (files.length > 0) {
         var timestamp = Date.now() - startTime;
-        sensorBuffer.push( timestamp + ', ' + x + ', ' + y + ', ' + z );
-        if ( sensorBuffer.length == 10 ) {
-          writeFile( files[ SENSORID ], sensorBuffer.join( '\n' ) + '\n', true );
+        sensorBuffer.push(timestamp + '; ' + x + '; ' + y + '; ' + z);
+        if (sensorBuffer.length == 10) {
+          writeFile(files[SENSORID], sensorBuffer.join('\n') + '\n', true);
           sensorBuffer = [];
         }
       }
     }
 
-    function logResult( msg ) {
-      if ( files.length > 0 ) {
-        writeFile( files[ RESULTID ], msg + '\n', true );
+    function logResult(msg) {
+      if (files.length > 0) {
+        writeFile(files[RESULTID], msg + '\n', true);
       }
     }
 
-    function logAnswer( comp, question, answer, correctAnswer ) {
+    function logAnswer(comp, question, answer, correctAnswer) {
       var msg = "";
-      msg += ( "Komponente " + comp + ', Frage ' + question + ': ' + answer + ' (richtige Antwort: ' + correctAnswer + ')' );
-      console.log( msg );
-      logResult( msg );
+      msg += ("Komponente " + comp + ', Frage ' + question + ': ' + answer + ' (richtige Antwort: ' + correctAnswer + ')');
+      console.log(msg);
+      logResult(msg);
     }
 
-    function writeFile( fileEntry, dataObj, isAppend ) {
+    function writeFile(fileEntry, dataObj, isAppend) {
       // Create a FileWriter object for our FileEntry (log.txt).
-      fileEntry.createWriter( function( fileWriter ) {
+      fileEntry.createWriter(function(fileWriter) {
 
         fileWriter.onwriteend = function() {
-          console.log( "Successful file read..." );
-          readFile( fileEntry );
+          console.log("Successful file read...");
+          readFile(fileEntry);
         };
 
-        fileWriter.onerror = function( e ) {
-          console.log( "Failed file read: " + e.toString() );
+        fileWriter.onerror = function(e) {
+          console.log("Failed file read: " + e.toString());
         };
 
         // If we are appending data to file, go to the end of the file.
-        if ( isAppend ) {
+        if (isAppend) {
           try {
-            fileWriter.seek( fileWriter.length );
-          } catch ( e ) {
-            console.log( "file doesn't exist!" );
+            fileWriter.seek(fileWriter.length);
+          } catch (e) {
+            console.log("file doesn't exist!");
           }
         }
-        fileWriter.write( dataObj );
-      } );
+        fileWriter.write(dataObj);
+      });
     }
 
-    function countMonths( day, month, year ) {
+    function countMonths(day, month, year) {
       startTime = new Date();
 
       var dayNow = startTime.getDate();
       var monthNow = startTime.getMonth() + 1;
       var yearNow = startTime.getFullYear();
-      var bthDate = new Date( year, month - 1, day );
+      var bthDate = new Date(year, month - 1, day);
       var ageYears, ageMonths, ageDays;
       //
       // function leapYear( yr ) {
@@ -131,62 +131,62 @@ angular.module( 'ATEM-App.services', [] )
       // }
 
       ageYears = startTime.getFullYear() - year;
-      ageMonths = startTime.getMonth() - ( month - 1 );
+      ageMonths = startTime.getMonth() - (month - 1);
       ageDays = startTime.getDate() - day;
-      if ( ageDays < 0 ) ageMonths = ageMonths - 1;
-      if ( ageMonths < 0 ) {
+      if (ageDays < 0) ageMonths = ageMonths - 1;
+      if (ageMonths < 0) {
         ageYears = ageYears - 1;
         ageMonths = ageMonths + 12;
       }
-      console.log( "alter: " + ( ageYears * 12 + ageMonths ) + " Monate" );
-      return ( ageYears * 12 + ageMonths );
+      console.log("alter: " + (ageYears * 12 + ageMonths) + " Monate");
+      return (ageYears * 12 + ageMonths);
     }
 
-    function createFile( fileName, fileText, fileID ) {
-      var fileDir = cordova.file.externalDataDirectory.replace( cordova.file.externalRootDirectory, '' );
+    function createFile(fileName, fileText, fileID) {
+      var fileDir = cordova.file.externalDataDirectory.replace(cordova.file.externalRootDirectory, '');
       var filePath = fileDir + fileName;
-      console.log( fileDir );
-      console.log( cordova.file.externalDataDirectory );
-      filesystem.root.getFile( filePath, {
+      console.log(fileDir);
+      console.log(cordova.file.externalDataDirectory);
+      filesystem.root.getFile(filePath, {
         create: true,
         exclusive: true
-      }, function( fileEntry ) {
-        console.log( 'fileEntry: ' + fileEntry );
-        files[ fileID ] = fileEntry;
-        writeFile( fileEntry, fileText, false );
-      }, function( e ) {
-        console.log( 'Error1' + e.code );
-      } );
+      }, function(fileEntry) {
+        console.log('fileEntry: ' + fileEntry);
+        files[fileID] = fileEntry;
+        writeFile(fileEntry, fileText, false);
+      }, function(e) {
+        console.log('Error1' + e.code);
+      });
     }
 
-    function readFile( fileEntry ) {
+    function readFile(fileEntry) {
 
-      fileEntry.file( function( file ) {
+      fileEntry.file(function(file) {
         var reader = new FileReader();
 
         reader.onloadend = function() {};
 
-        reader.readAsText( file );
+        reader.readAsText(file);
 
-      }, function( err ) {
-        console.log( 'error' + err.code );
-      } );
+      }, function(err) {
+        console.log('error' + err.code);
+      });
     }
 
-    function logStart( day, month, year, gender, language ) {
-      var ageInMonths = countMonths( day, month, year );
+    function logStart(day, month, year, gender, language) {
+      var ageInMonths = countMonths(day, month, year);
       var userID = Date.now();
-      createFile( "sensor" + userID + ".csv", 'Timestamp;X;Y;Z' + '\n', SENSORID );
-      createFile( "events" + userID + ".txt", '', EVENTID );
-      createFile( "results" + userID + ".txt", "Alter: " + ageInMonths + " Monate" + '\n' + "Geschlecht: " + gender + '\n' + "Sprache: " + language + '\n', RESULTID );
+      createFile("sensor" + userID + ".csv", 'Timestamp;X;Y;Z' + '\n', SENSORID);
+      createFile("events" + userID + ".txt", '', EVENTID);
+      createFile("results" + userID + ".txt", "Alter: " + ageInMonths + " Monate" + '\n' + "Geschlecht: " + gender + '\n' + "Sprache: " + language + '\n', RESULTID);
     }
 
     function logEnd() {
-      if ( files.length > 0 ) {
+      if (files.length > 0) {
         var endTime = Date.now();
-        logEvent( 'Test finished', 0, 0 );
-        clearInterval( sensorInterval );
-        writeFile( files[ SENSORID ], sensorBuffer.join( '\n' ) + '\n', true );
+        logEvent('Test finished', 0, 0);
+        clearInterval(sensorInterval);
+        writeFile(files[SENSORID], sensorBuffer.join('\n') + '\n', true);
         sensorBuffer = [];
         //logResult("Bearbeitungszeit: " + printTime(startTime - endTime));
         files = [];
@@ -215,26 +215,26 @@ angular.module( 'ATEM-App.services', [] )
     }
 
     function startSensor() {
-      logEvent( 'Start', 0, 0 );
-      sensorInterval = setInterval( logSensor, 20 );
-      window.addEventListener( 'pause', function() {
-        logEvent( 'Test paused', 0, 0 );
-        clearInterval( sensorInterval );
-        window.addEventListener( 'resume', function() {
-          logEvent( 'Test continued', 0, 0 );
-          sensorInterval = setInterval( logSensor, 20 );
-        } )
-      } )
+      logEvent('Start', 0, 0);
+      sensorInterval = setInterval(logSensor, 20);
+      window.addEventListener('pause', function() {
+        logEvent('Test paused', 0, 0);
+        clearInterval(sensorInterval);
+        window.addEventListener('resume', function() {
+          logEvent('Test continued', 0, 0);
+          sensorInterval = setInterval(logSensor, 20);
+        })
+      })
     }
 
     function wipeData() {
       results = [];
-      clearInterval( sensorInterval );
+      clearInterval(sensorInterval);
     }
 
-    function loadJson( id ) {
+    function loadJson(id) {
       var filepath = "json/" + id + ".json";
-      return $http.get( filepath );
+      return $http.get(filepath);
     }
 
     return {
@@ -248,26 +248,26 @@ angular.module( 'ATEM-App.services', [] )
       loadJson: loadJson
     };
 
-  } ] )
+  }])
 
-  .service( 'audioService', function() {
+  .service('audioService', function() {
 
     var my_media;
 
-    function playAudio( url ) {
+    function playAudio(url) {
       // Play the audio file at url
-      if ( my_media != null ) my_media.release();
+      if (my_media != null) my_media.release();
 
       url = "/android_asset/www/" + url;
       //fix url for Android
-      my_media = new Media( url,
+      my_media = new Media(url,
         // success callback
         function() {
-          console.log( "playAudio():Audio Success" );
+          console.log("playAudio():Audio Success");
         },
         // error callback
-        function( err ) {
-          console.log( "playAudio():Audio Error: " + err.code + err.message );
+        function(err) {
+          console.log("playAudio():Audio Error: " + err.code + err.message);
         }
       );
 
@@ -279,7 +279,7 @@ angular.module( 'ATEM-App.services', [] )
       my_media.stop();
     }
 
-    function repeatAudio( url ) {
+    function repeatAudio(url) {
       my_media.stop();
       my_media.play();
     }
@@ -289,4 +289,4 @@ angular.module( 'ATEM-App.services', [] )
       stopAudio: stopAudio,
       repeatAudio: repeatAudio
     };
-  } );
+  });
